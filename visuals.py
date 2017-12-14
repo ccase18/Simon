0f0bd9b3a9b4863b182709a079441be027e05a5c
Canaan Case <canaancase@Canaans-MacBook-Pro.local>
2017-12-14 15:42:32 -0500

    added new time and visuals files

---
+++ visuals.py
@@ -0,0 +1,11 @@
+class visuals:
+	def __init__(self):
+
+	def window(self):
+		#determines the size of the window and creates the actual squares appearance. 
+
+	def tkinter(self):
+		#uses tkinter to help create the window and flashing
+
+	def flash(self):
+		#flashes a given square 
