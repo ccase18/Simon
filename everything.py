fc8cf51d76425f4f0203920ac68a906efe0fd663
kevinchyun3502 <32549493+kevinchyun3502@users.noreply.github.com>
2017-12-19 13:31:16 -0500

    ddd

--- everything.py
+++ everything.py
@@ -8,15 +8,7 @@ class Game
 
 		self.Current_Score = s
 		self.Game_Status = True 
-		self.board = [ 
-		[Cell("A1"), Cell("B1")],
-		[Cell("A2"), Cell("B2")]		
-		self.A1 = self.board[0][0] 
-		self.A2 = self.board[1][0]
-		self.B1 = self.board[0][1]
-		self.B2 = self.board[1][1]
-		]
-
+		
 	def input(): 
 		list[]	{
 		A1 = '1'
@@ -45,39 +37,6 @@ class Game
 	    global r
 	    global sequence
 
-
-
-def User_Input():
-	A1 = Q
-	A2 = W
-	A3 = A 
-	A4 = S
-	
-#The user will be able to select on certain square by clicking these keys
-#Q is for the top left square. 
-#W is for the top right square.
-#A is for the bottom left square.
-#S is for the bottom right square. 
-
-def check_Sequence():
-	global sequence
-
-	if user_input == sequence:
-		scoreUp()
-
-	user_input = str(input('Please click the tiles our AI showed you. Click Q for the top left square. Press W for top right square. Press A for bottom left square. Press S for bottom right sqaure.'))
-		
-	if user_input == sequence:
-		s += 5
-	elif user_input != sequence:
-		s == s
-		endgame()
-
-#The user will be able to select on certain square by clicking these keys
-#Q is for the top left square. 
-#W is for the top right square.
-#A is for the bottom left square.
-#S is for the bottom right square.
 
 import time
 
@@ -156,6 +115,32 @@ board = Visuals(root)
 board.drawRectangles()
 root.mainLoop()
 
+	
+#The user will be able to select on certain square by clicking these keys
+#Q is for the top left square. 
+#W is for the top right square.
+#A is for the bottom left square.
+#S is for the bottom right square. 
+
+def check_Sequence():
+	global sequence
+
+	if user_input == sequence:
+		scoreUp()
+
+	user_input = str(input('Please click the numbers between 1 and 4 our AI showed you just now. Click Q for the top left square. Press W for top right square. Press A for bottom left square. Press S for bottom right sqaure.'))
+		
+	if user_input == sequence:
+		s += 5
+	elif user_input != sequence:
+		s == s
+		endgame()
+
+#The user will be able to select on certain square by clicking these keys
+#Q is for the top left square. 
+#W is for the top right square.
+#A is for the bottom left square.
+#S is for the bottom right square.
 
 
 
